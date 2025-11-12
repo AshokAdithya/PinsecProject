41c29dc0f7c9a12d7b1a208f01dc2a5029676faa
AshokAdithya <ashokadithya2006@gmail.com>
2025-11-12 17:59:44 +0530

    Updated retry logic

--- app/config/binance_symbols.py
+++ app/config/binance_symbols.py
@@ -9,24 +9,42 @@ load_dotenv()
 VALID_SYMBOLS = set()
 _lock = asyncio.Lock()
 
-async def fetch_testnet_symbols():
+
+async def fetch_testnet_symbols(retries: int = 3, delay=2.0):
     url = os.getenv("BINANCE_LIST")
 
-    try:
-        response = await asyncio.to_thread(requests.get, url, timeout=5)
-        response.raise_for_status()
-        data = response.json()
+    for attempt in range(1, retries + 1):
+        try:
+            response = await asyncio.to_thread(requests.get, url, timeout=5)
+            response.raise_for_status()
+            data = response.json()
 
-        symbols = {s["symbol"].upper() for s in data.get("symbols", []) if s.get("status") == "TRADING"}
+            symbols = {
+                s["symbol"].upper()
+                for s in data.get("symbols", [])
+                if s.get("status") == "TRADING"
+            }
 
-        async with _lock:
-            VALID_SYMBOLS.clear()
-            VALID_SYMBOLS.update(symbols)
+            async with _lock:
+                VALID_SYMBOLS.clear()
+                VALID_SYMBOLS.update(symbols)
 
-        logger.info(f"Loaded {len(VALID_SYMBOLS)} symbols from Binance Testnet")
+            logger.info(f"Loaded {len(VALID_SYMBOLS)} symbols from Binance Testnet")
+            return
 
-    except Exception as e:
-        logger.error(f"Failed to fetch symbols: {e}")
+        except Exception as e:
+
+            logger.error(f"Attempt {attempt}/{retries} failed to fetch symbols: {e}")
+
+            if attempt < retries:
+                sleep_time = delay * (
+                    2 ** (attempt - 1)
+                )  # Retry logic with increasing delay
+                logger.info(f"Retrying in {sleep_time:.1f}s...")
+                await asyncio.sleep(sleep_time)
+            else:
+                logger.error("❌ All retry attempts failed.")
+
 
 def is_valid_symbol(symbol: str) -> bool:
     return symbol.upper() in VALID_SYMBOLS
--- websocket_client/ws_client.py
+++ websocket_client/ws_client.py
@@ -1,9 +1,17 @@
 # ws_client.py
 import asyncio
 import websockets
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+WEBSOCKET_HOST = os.getenv("WEBSOCKET_HOST")
+WEBSOCKET_PORT = os.getenv("WEBSOCKET_PORT")
+
 
 async def listen():
-    uri = "ws://localhost:8765"
+    uri = f"ws://localhost:{WEBSOCKET_PORT}"
 
     async with websockets.connect(uri) as websocket:
         print("Connected to Crypto Price Streaming Platform WebSocket server!")
@@ -11,6 +19,7 @@ async def listen():
         # Continuously read messages
         async for message in websocket:
             print(f"Candle: {message}")
+
 
 if __name__ == "__main__":
     asyncio.run(listen())
